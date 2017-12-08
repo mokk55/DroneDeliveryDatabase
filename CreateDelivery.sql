6fd4dfa98bdf680fa1f140528b465e2409340689
Alex <threepointonefourfive@gmail.com>
2017-12-08 13:32:49 -0800

    changes to Drones

--- CreateDelivery.sql
+++ CreateDelivery.sql
@@ -1,12 +1,3 @@
-USE delivery;
-
-DROP TABLE Status;
-DROP TABLE DroneStatus;
-DROP TABLE Orders;
-DROP TABLE RoutePosition;
-DROP TABLE Routes;
-DROP TABLE Drones;
-
 DROP DATABASE delivery;
 
 CREATE DATABASE delivery;
@@ -59,11 +50,11 @@ CREATE TABLE RoutePosition (
 
 CREATE TABLE Drones (
     DroneID int NOT NULL auto_increment,
-    OrderID int NOT NULL,
-    RoutesKey int NOT NULL,
+    OrderKey int,
+    RoutesKey int,
     DroneStatusKey int NOT NULL,
     PRIMARY KEY (DroneID),
-    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
+    FOREIGN KEY (OrderKey) REFERENCES Orders(OrderID),
     FOREIGN KEY (RoutesKey) REFERENCES RoutePosition(RoutesKey),
     FOREIGN KEY (DroneStatusKey) REFERENCES DroneStatus(DroneStatusID)
 );
@@ -76,12 +67,10 @@ INSERT INTO status(StatusDesc) values('Complete');
 INSERT INTO status(StatusDesc) values('Failed');
 INSERT INTO status(StatusDesc) values('Archived');
 
-INSERT INTO `delivery`.`orders` (`SrcX`, `SrcY`, `SrcZ`, `DestX`, `DestY`, `DestZ`, `StatusKey`) VALUES ('2', '2', '2', '35.3', '63.12', '2.69', '2');
-INSERT INTO `delivery`.`orders` (`SrcX`, `SrcY`, `SrcZ`, `DestX`, `DestY`, `DestZ`, `StatusKey`) VALUES ('2', '2', '2', '34', '125', '25', '2');
-INSERT INTO `delivery`.`orders` (`SrcX`, `SrcY`, `SrcZ`, `DestX`, `DestY`, `DestZ`, `StatusKey`) VALUES ('2', '2', '2', '24', '61', '52', '2');
-INSERT INTO `delivery`.`orders` (`SrcX`, `SrcY`, `SrcZ`, `DestX`, `DestY`, `DestZ`, `StatusKey`) VALUES ('2', '2', '2', '2.52', '3.246', '3', '2');
-INSERT INTO `delivery`.`orders` (`SrcX`, `SrcY`, `SrcZ`, `DestX`, `DestY`, `DestZ`, `StatusKey`) VALUES ('2', '2', '2', '2.35', '8', '2.63', '2');
-INSERT INTO `delivery`.`orders` (`SrcX`, `SrcY`, `SrcZ`, `DestX`, `DestY`, `DestZ`, `StatusKey`) VALUES ('2', '2', '2', '0.34', '243', '6.356', '2');
-INSERT INTO `delivery`.`orders` (`SrcX`, `SrcY`, `SrcZ`, `DestX`, `DestY`, `DestZ`, `StatusKey`) VALUES ('10.5', '10.5', '10.5', '29.63', '2.346', '34', '2');
-INSERT INTO `delivery`.`orders` (`SrcX`, `SrcY`, `SrcZ`, `DestX`, `DestY`, `DestZ`, `StatusKey`) VALUES ('10.5', '10.5', '10.5', '3.62', '34.23', '68', '2');
-INSERT INTO `delivery`.`orders` (`SrcX`, `SrcY`, `SrcZ`, `DestX`, `DestY`, `DestZ`, `StatusKey`) VALUES ('10.5', '10.5', '10.5', '93.2', '6', '90', '2');
+INSERT INTO DroneStatus(StatusDesc) values('Charging');
+INSERT INTO DroneStatus(StatusDesc) values('Waiting');
+INSERT INTO DroneStatus(StatusDesc) values('Loading');
+INSERT INTO status(StatusDesc) values('En Route');
+INSERT INTO DroneStatus(StatusDesc) values('Delivering');
+INSERT INTO DroneStatus(StatusDesc) values('Returning');
+INSERT INTO DroneStatus(StatusDesc) values('Crashed');
